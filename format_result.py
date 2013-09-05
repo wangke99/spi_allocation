489df88567d466ab5acef9cbb3c79d2c96d98b0a
Ke Wang <kewang55@gmail.com>
2013-09-05 11:14:03 -0700

    format result in development

--- format_result.py
+++ format_result.py
@@ -1,2 +1,18 @@
-import json
+from pandas import DataFrame, Series
+import pandas as pd
+
+config = pd.read_csv('output/config.txt', header=None, sep='\t')
+config.columns = ['spi_file', 'status', 'spi']
+
+unfinished = config[config['status'] == 0]
+
+config = config[config['status'] == 1]
+config['bucket'] = config['spi_file'].map(lambda x: x.replace('input/','').replace('_spi.json',''))
+config['output'] = config['bucket'].map(lambda x: 'output/'+x+'_output.txt')
+config['account_mapper'] = config['bucket'].map(lambda x: 'input/'+x+'_account_map.txt')
+config['rep_mapper'] = config['bucket'].map(lambda x: 'input/'+x+'_rep_map.txt')
+config['output_file'] = config['bucket'].map(lambda x: 'final_output/'+x+'_final_output.txt')
+
+
+print config
 
