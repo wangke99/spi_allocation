ca4af2fbfa49f9656bc53fef805c0cbbc5132d38
Ke Wang <kewang55@gmail.com>
2013-09-05 12:33:07 -0700

    output formatting updated

--- format_result.py
+++ format_result.py
@@ -1,4 +1,4 @@
-from pandas import DataFrame, Series
+from pandas import DataFrame, SeriesAOAOA
 import pandas as pd
 
 config = pd.read_csv('output/config.txt', header=None, sep='\t')
@@ -14,5 +14,36 @@ config['rep_mapper'] = config['bucket'].map(lambda x: 'input/'+x+'_rep_map.txt')
 config['output_file'] = config['bucket'].map(lambda x: 'final_output/'+x+'_final_output.txt')
 
 
-print config
+config = config[['bucket', 'output', 'account_mapper', 'rep_mapper', 'output_file']]
 
+
+#print config
+
+for each in config.itertuples():
+    bucket = each[1]
+    output = each[2]
+    account_mapper = each[3]
+    rep_mapper = each[4]
+    output_file = each[5]
+
+    #print output
+    
+    d = pd.read_csv(output, header=None, sep=',')
+    d.columns=['account_index','rep_index']
+    a = pd.read_csv(account_mapper, header=None, sep='\t')
+    a.columns=['account', 'account_index']
+    r = pd.read_csv(rep_mapper, header=None, sep='\t')
+    r.columns=['rep', 'rep_index']
+
+    output_data = d.merge(a, left_on = ['account_index'], right_on = ['account_index'])
+    output_data = output_data.merge(r, left_on=['rep_index'], right_on = ['rep_index'])
+    output_data = output_data[['account', 'rep']]
+    
+    print '[INFO] Wrting allocation plan for bucket: %s' % (bucket)
+    print '[INFO] %s - %s accounts are allocated' % (bucket, str(len(output_data)))
+    output_data.to_csv(output_file, header=None, sep=',', index=False)
+
+
+print '[INFO] Updating output config file at output/config.txt'
+print '[INFO] %s buckets remaining' % (len(unfinished))
+unfinished.to_csv('output/config.txt', header=None, sep='\t', index=False)
